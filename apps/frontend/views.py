dffc10808b3932984313535965531febe35a5445
Patrick Hladun <patrickhladun@gmail.com>
2024-08-02 20:45:49 +0100

    feat: add about page

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "frontend/index.html")
+
+
+def about(request):
+    return render(request, "frontend/about.html")
