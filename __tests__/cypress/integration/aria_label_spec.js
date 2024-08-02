359049436703534df956567825059a9db82fac3b
Patrick Hladun <patrickhladun@gmail.com>
2024-08-02 20:57:47 +0100

    test: split aria-label tests to footer and header

--- __tests__/cypress/integration/aria_label_spec.js
+++ __tests__/cypress/integration/aria_label_spec.js
@@ -1,8 +1,16 @@
 describe("Accessibility Tests", () => {
-  it("All <a> tags must have an aria-label attribute", () => {
+  it("All header <a> tags must have an aria-label attribute", () => {
     cy.visit("http://127.0.0.1:8000/");
 
-    cy.get("a").each(($el) => {
+    cy.get("body > header a").each(($el) => {
+      expect($el).to.have.attr("aria-label");
+    });
+  });
+
+  it("All footer <a> tags must have an aria-label attribute", () => {
+    cy.visit("http://127.0.0.1:8000/");
+
+    cy.get("body > footer a").each(($el) => {
       expect($el).to.have.attr("aria-label");
     });
   });
