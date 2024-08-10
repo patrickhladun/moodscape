2bf9c987a2ea88fa05c8467b0087bcc5e6750ed4
Patrick Hladun <patrickhladun@gmail.com>
2024-08-10 10:49:42 +0100

    feat: add product and category models

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from apps.user.models import User
 
-# Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    details = models.TextField()
+    sku = models.CharField(max_length=255, unique=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    featured = models.ImageField(upload_to="products/", null=True, blank=True)
+    is_draft = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "mood_product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f"{self.sku} - {self.name}"
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "mood_category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
