67d230c77706375485d7013e8fb48b3bd02697a2
Patrick Hladun <patrickhladun@gmail.com>
2024-08-11 21:13:42 +0100

    fix: update url's

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", include("apps.frontend.urls")),
+    path("", include("apps.product.urls")),
     path("", include("apps.user.urls")),
     path('admin/', admin.site.urls),
     path('account/', include('allauth.urls')),
 ]
+
+urlpatterns += [path('summernote/', include('django_summernote.urls'))]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
