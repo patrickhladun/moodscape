925f3ff5c416c05941cccdb37674f6c7ff445fa9
Patrick Hladun <patrickhladun@gmail.com>
2024-08-12 17:27:42 +0100

    test: fix tests by passing active in context

--- __tests__/pytest/ui/test_admin_navigation.py
+++ __tests__/pytest/ui/test_admin_navigation.py
@@ -5,7 +5,7 @@ from django.test import RequestFactory
 @pytest.mark.django_db
 def test_admin_navigation_active_account():
     request = RequestFactory().get('/account/')
-    rendered_template = render_to_string('partials/nav_admin.html', {'request': request})
+    rendered_template = render_to_string('partials/nav_admin.html', {'request': request, 'active': 'account'})
     
     assert 'class="admin-nav-item active"' in rendered_template
     assert 'href="/account/"' in rendered_template
@@ -16,8 +16,7 @@ def test_admin_navigation_active_account():
 @pytest.mark.django_db
 def test_admin_navigation_active_products():
     request = RequestFactory().get('/account/products/')
-    
-    rendered_template = render_to_string('partials/nav_admin.html', {'request': request})
+    rendered_template = render_to_string('partials/nav_admin.html', {'request': request, 'active': 'products'})
     
     assert 'class="admin-nav-item active"' in rendered_template
     assert 'href="/account/products/"' in rendered_template
