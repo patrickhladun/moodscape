58658a01f42237dfae8368725903bf50aa8c99fb
Patrick Hladun <patrickhladun@gmail.com>
2024-08-22 10:45:03 +0100

    feat: allow admin to update orders

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('account/orders/', views.orders_view, name='admin_orders'),
+    path('account/orders/<order_number>/update/', 
+        views.order_update_view, name='admin_order_update'),
 ]
