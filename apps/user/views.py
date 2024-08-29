987b98e1e70e2f963711b0bf813860be2a5e0490
Patrick Hladun <patrickhladun@gmail.com>
2024-08-29 18:43:51 +0100

    feat: add customer edit page

--- apps/user/views.py
+++ apps/user/views.py
@@ -7,7 +7,7 @@ from constance import config
 from apps.common.decorators import superuser_required
 
 from .models import Customer
-from .forms import AccountProfileForm
+from .forms import AccountProfileForm, CustomerProfileForm
 
 @login_required
 @superuser_required
@@ -21,6 +21,30 @@ def cms_customers_view(request):
         'customers': customers 
     }
     return render(request, template, context)
+
+
+@login_required
+@superuser_required
+def cms_customer_update_view(request, id):
+    customer = get_object_or_404(Customer, id=id)
+
+    if request.method == "POST":
+        form = CustomerProfileForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Customer details updated successfully.")
+            return redirect('cms_customers')
+    else:
+        form = CustomerProfileForm(instance=customer)
+
+    template = "user/cms/customer_update.html"
+    context = {
+        "active": "customers",
+        "customer": customer,
+        "form": form
+    }
+    return render(request, template, context)
+
 
 @login_required
 def account_view(request):
