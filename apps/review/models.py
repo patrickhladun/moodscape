85c08be2cb30960db515bf574d854d3610c1958b
Patrick Hladun <patrickhladun@gmail.com>
2024-09-05 13:45:28 +0100

    feat: add validators

--- apps/review/models.py
+++ apps/review/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from apps.product.models import Product
 from apps.order.models import OrderLineItem
 from apps.user.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Review(models.Model):
     STATUS_CHOICES = [
@@ -15,8 +16,9 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, related_name="user_reviews", on_delete=models.CASCADE)
     order_line_item = models.ForeignKey(OrderLineItem, on_delete=models.CASCADE, related_name='lineitem_reviews')
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     text = models.TextField(max_length=160)
+    comment = models.TextField(blank=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
