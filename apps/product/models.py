b4e40d93211d65c2ec062cfeb364476891dbe8f9
Patrick Hladun <patrickhladun@gmail.com>
2024-09-24 14:44:40 +0100

    feat: add function to rename featured image on upload

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,5 +1,23 @@
+import os
+import random
+import string
+from datetime import datetime
 from django.db import models
 from apps.user.models import User
+
+
+def featured_image_path(instance, filename):
+    """
+    Generate a file upload path with a random four-character string and the product ID in the filename.
+    """
+    timestamp = datetime.now().strftime("%y%m%d")
+    extension = os.path.splitext(filename)[1]
+    rand_chars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=4))
+    new_filename = f"featured_{timestamp}_{rand_chars}{extension}"
+    path = "products/"
+    return os.path.join(path, new_filename)
+
+
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,7 +28,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=255, unique=True)
     stock = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    featured = models.ImageField(upload_to="products/", null=True, blank=True)
+    featured = models.ImageField(upload_to=featured_image_path, null=True, blank=True)
     is_published = models.BooleanField(default=False)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
