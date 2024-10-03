d0a87c82910526c33aed5a0de06a6df948d32758
Patrick Hladun <patrickhladun@gmail.com>
2024-10-03 17:57:18 +0100

    feat: populate checkout field for logged in user

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -98,11 +98,37 @@ def checkout_view(request):
         else:
             messages.error(request, 'There was an error with your form. Please double check your information.')
 
+        # If it's a POST request, we don't need to generate a Stripe payment intent
+        client_secret = ''
     else:
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('shop'))
+
+        # Pre-populate form if the user is logged in
+        if request.user.is_authenticated:
+            try:
+                customer = request.user.customer  # Assuming you have a customer relationship
+                initial_data = {
+                    'first_name': customer.first_name,
+                    'last_name': customer.last_name,
+                    'phone_number': customer.phone_number,
+                    'country': customer.country,
+                    'postcode': customer.postcode,
+                    'town_city': customer.town_city,
+                    'address_line_1': customer.address_line_1,
+                    'address_line_2': customer.address_line_2,
+                    'county': customer.county,
+                    'email': request.user.email
+                }
+            except AttributeError:
+                # If there's no Customer record, use only the user email
+                initial_data = {'email': request.user.email}
+        else:
+            initial_data = {}
+
+        order_form = CreateOrderForm(initial=initial_data)
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
@@ -113,7 +139,7 @@ def checkout_view(request):
             currency=config.CURRENCY,
         )
 
-        order_form = CreateOrderForm()
+        client_secret = intent.client_secret
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
@@ -124,9 +150,11 @@ def checkout_view(request):
         "metadata": metadata,
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'client_secret': client_secret,  # Pass an empty client_secret if it's a POST request
     }
     return render(request, template, context)
+
+
 
 
 def checkout_success_view(request, order_number):
