598d3b8d05b4d731024387d794a04733532d7fe6
Patrick Hladun <patrickhladun@gmail.com>
2024-10-03 13:34:32 +0100

    test: write tests for the website footer

--- __tests__/cypress/e2e/footer_accesibility_spec.js
+++
@@ -1,9 +0,0 @@
-describe("Header Accessibility Tests", () => {
-  it("All footer <a> tags must have an aria-label attribute", () => {
-    cy.visit("/");
-
-    cy.get("body > footer a").each(($el) => {
-      expect($el).to.have.attr("aria-label");
-    });
-  });
-});
