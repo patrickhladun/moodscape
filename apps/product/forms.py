dfac8d1f150a60b5f76e25c8f4dc200e45a6102a
Patrick Hladun <patrickhladun@gmail.com>
2024-10-05 16:33:29 +0100

    chore: validate, format and document code in product app

--- apps/product/forms.py
+++ apps/product/forms.py
@@ -3,42 +3,57 @@ from django.utils.text import slugify
 from .models import Product, Category
 from django_summernote.widgets import SummernoteWidget
 
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'slug', 'details', 'sku', 'stock', 'price', 'featured', 'category', 'is_published']
+        fields = [
+            "name",
+            "slug",
+            "details",
+            "sku",
+            "stock",
+            "price",
+            "featured",
+            "category",
+            "is_published",
+        ]
         widgets = {
-            'details': SummernoteWidget(),
+            "details": SummernoteWidget(),
         }
 
     def clean_slug(self):
-        slug = self.cleaned_data.get('slug')
-        name = self.cleaned_data.get('name')
-        
+        slug = self.cleaned_data.get("slug")
+        name = self.cleaned_data.get("name")
+
         if not slug:
             slug = slugify(name)
 
-        queryset = Product.objects.filter(slug=slug).exclude(id=self.instance.id)
+        queryset = Product.objects.filter(slug=slug).exclude(
+            id=self.instance.id
+        )
         if queryset.exists():
-            raise forms.ValidationError("A product with this slug already exists. Please choose a different name or provide a unique slug.")
+            raise forms.ValidationError(
+                "A product with this slug already exists. Please choose a \
+                different name or provide a unique slug."
+            )
 
         return slug
 
     def clean_stock(self):
-        stock = self.cleaned_data.get('stock')
+        stock = self.cleaned_data.get("stock")
         if stock < 0:
             raise forms.ValidationError("Stock cannot be negative.")
         return stock
 
     def clean_price(self):
-        price = self.cleaned_data.get('price')
+        price = self.cleaned_data.get("price")
         if price < 0 or price == 0:
             raise forms.ValidationError("Price cannot be negative.")
         return price
 
 
-
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name', 'slug', 'description']
+        fields = ["name", "slug", "description"]
--- apps/product/signals.py
+++ apps/product/signals.py
@@ -5,20 +5,27 @@ from django.db.models import Avg
 from apps.review.models import Review
 from .models import Product
 
+
 def update_product_rating(product):
-    """ Helper function to update the product's rating. """
-    new_rating = Review.objects.filter(product=product, status='approved').aggregate(Avg('rating'))['rating__avg']
+    """Helper function to update the product's rating."""
+    new_rating = Review.objects.filter(
+        product=product, status="approved"
+    ).aggregate(Avg("rating"))["rating__avg"]
     product.rating = round(new_rating, 1) if new_rating else None
-    product.save(update_fields=['rating'])
+    product.save(update_fields=["rating"])
+
 
 @receiver(post_save, sender=Review)
 def update_product_rating_on_save(sender, instance, created, **kwargs):
-    """ Update the product rating if a review's approval status changes. """
-    if instance.status == 'approved' or (not created and 'status' in instance.tracker.changed()):
+    """Update the product rating if a review's approval status changes."""
+    if instance.status == "approved" or (
+        not created and "status" in instance.tracker.changed()
+    ):
         update_product_rating(instance.product)
+
 
 @receiver(post_delete, sender=Review)
 def update_product_rating_on_delete(sender, instance, **kwargs):
-    """ Update the product rating when an approved review is deleted. """
-    if instance.status == 'approved':
+    """Update the product rating when an approved review is deleted."""
+    if instance.status == "approved":
         update_product_rating(instance.product)
