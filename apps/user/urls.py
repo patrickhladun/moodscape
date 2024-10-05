01c27ae8383234042a0fc53f274beffa355cc8ef
Patrick Hladun <patrickhladun@gmail.com>
2024-10-05 16:44:45 +0100

    chore: validate, format and document code in user app

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('cms/customers/', views.cms_customers_view, name='cms_customers'),
-    path('cms/customers/<int:id>/update/', views.cms_customer_update_view, name='cms_customer_update'),
-    path("cms/customers/<int:id>/delete/", views.cms_customer_delete_view, name="cms_customer_delete"),
-    path('account/', views.account_view, name='account'),
-    path('account/profile/', views.account_profile_view, name='account_profile'),
+    path("cms/customers/", views.cms_customers_view, name="cms_customers"),
+    path(
+        "cms/customers/<int:id>/update/",
+        views.cms_customer_update_view,
+        name="cms_customer_update",
+    ),
+    path(
+        "cms/customers/<int:id>/delete/",
+        views.cms_customer_delete_view,
+        name="cms_customer_delete",
+    ),
+    path("account/", views.account_view, name="account"),
+    path(
+        "account/profile/",
+        views.account_profile_view,
+        name="account_profile",
+    ),
 ]
