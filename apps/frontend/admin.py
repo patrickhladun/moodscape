2d2bd8a3ae901a1bfa3e785d6cfe67c4326c149d
Patrick Hladun <patrickhladun@gmail.com>
2024-10-17 09:20:03 +0100

    feat: add faq management in django admin

--- apps/frontend/admin.py
+++ apps/frontend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage, FAQ, FAQSection
 
 
 class ContactMessageAdmin(admin.ModelAdmin):
@@ -8,4 +8,14 @@ class ContactMessageAdmin(admin.ModelAdmin):
     readonly_fields = ("name", "email", "message", "created_at")
 
 
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ("question", "section", "created_at")
+
+
+class FAQSectionAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+
+
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(FAQSection, FAQSectionAdmin)
 admin.site.register(ContactMessage, ContactMessageAdmin)
